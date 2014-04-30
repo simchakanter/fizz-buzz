5b0b43bd8bcb88684d21abae71cb3aa68ce02cdf
Simcha Kanter <simchakanter@gmail.com>
2014-04-30 07:48:48 -0500

    Finish refactor with error messages and validation for strings and decimals

--- js/app.js
+++ js/app.js
@@ -5,31 +5,41 @@ $(document).ready(function() {
   $('#getNumberForm').on('click', '#submitNumber', function(event) {
     event.preventDefault();
     maxNumber = +$('#getMaxNumber').val() + 1;
-    console.log("User entered " + maxNumber);
-    for (var i = 1; i < maxNumber; i++) {
-      if (i % 3 === 0) {
-        if (i % 5 === 0) {
-          results.push("fizzbuzz");
-        } else {
-          results.push("fizz");
-        }
-      }
-      else if (i % 5 === 0) {
+    $('#getMaxNumber').val("");
+    console.log("User entered " + (maxNumber - 1) + ", adding one to set upper limit...");
+    if (isNaN(maxNumber)) {
+      $('#error-message').text("That ain't no number.");
+      $('#error-message').show();
+    }
+    else if (maxNumber % 1 !== 0) {
+      $('#error-message').text("How am I supposed to count to a decimal?");
+      $('#error-message').show();
+    }
+    else {
+      $('#error-message').hide();
+      for (var i = 1; i < maxNumber; i++) {
         if (i % 3 === 0) {
-          results.push("fizzbuzz");
-        } else {
-          results.push("buzz");
+          if (i % 5 === 0) {
+            results.push("fizzbuzz");
+          } else {
+            results.push("fizz");
+          }
+        }
+        else if (i % 5 === 0) {
+          if (i % 3 === 0) {
+            results.push("fizzbuzz");
+          } else {
+            results.push("buzz");
+          }
+        }
+        else {
+          results.push(i);
         }
       }
-      else {
-        results.push(i);
+      for (var j = 0; j < results.length; j++) {
+        $('<li>' + results[j] + '</li>').appendTo('#list-of-numbers');
       }
     }
-    for (var j = 0; j < results.length; j++) {
-      $('<li>' + results[j] + '</li>').appendTo('#list-of-numbers');
-    }
-
-    //$('<p>' + results + '</p>').appendTo('body');
   });
 
 });
