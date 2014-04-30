869eef2569dc4a91bfe07aa51b32a20f24848a12
Simcha Kanter <simchakanter@gmail.com>
2014-04-29 21:10:49 -0500

    Start fizzbuzz refactor. Move to li's.  Add click handler.

--- js/app.js
+++ js/app.js
@@ -1,25 +1,35 @@
 var results = [];
+var maxNumber;
 
 $(document).ready(function() {
-  console.log("Test");
-  for (var i = 1; i < 101; i++) {
-    if (i % 3 === 0) {
-      if (i % 5 === 0) {
-        results.push("fizzbuzz");
-      } else {
-        results.push("fizz");
-      }
-    }
-    else if (i % 5 === 0) {
+  $('#getNumberForm').on('click', '#submitNumber', function(event) {
+    event.preventDefault();
+    maxNumber = +$('#getMaxNumber').val() + 1;
+    console.log("User entered " + maxNumber);
+    for (var i = 1; i < maxNumber; i++) {
       if (i % 3 === 0) {
-        results.push("fizzbuzz");
-      } else {
-        results.push("buzz");
+        if (i % 5 === 0) {
+          results.push("fizzbuzz");
+        } else {
+          results.push("fizz");
+        }
+      }
+      else if (i % 5 === 0) {
+        if (i % 3 === 0) {
+          results.push("fizzbuzz");
+        } else {
+          results.push("buzz");
+        }
+      }
+      else {
+        results.push(i);
       }
     }
-    else {
-      results.push(i);
+    for (var j = 0; j < results.length; j++) {
+      $('<li>' + results[j] + '</li>').appendTo('#list-of-numbers');
     }
-  }
-  $('<p>' + results + '</p>').appendTo('body');
+
+    //$('<p>' + results + '</p>').appendTo('body');
+  });
+
 });
