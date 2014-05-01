a747551485759a224c874fb0c0e35be60e817ee7
Simcha Kanter <simchakanter@gmail.com>
2014-05-01 14:41:43 -0500

    Clean up code, use shortcut for if statement

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ var maxNumber;
 $(document).ready(function() {
   $('#getNumberForm').on('click', '#submitNumber', function(event) {
     event.preventDefault();
-    maxNumber = +$('#getMaxNumber').val() + 1;
+    maxNumber = +$('#getMaxNumber').val();
     $('#getMaxNumber').val("");
-    console.log("User entered " + (maxNumber - 1) + ", adding one to set upper limit...");
+    console.log("User entered " + (maxNumber));
     if (isNaN(maxNumber)) {
       $('#error-message').text("That ain't no number.");
       $('#error-message').show();
@@ -25,24 +25,26 @@ $(document).ready(function() {
   });
 });
 
+// a = x ? foo : bar is a shortcut in JS to do
+// if (x) { a= foo} else {a = bar}
+
+
+
 function fizzBuzz (maxLimit) {
-  for (var i = 1; i < maxNumber; i++) {
-    if (i % 3 === 0) {
-      if (i % 5 === 0) {
-        results.push("fizzbuzz");
-      } else {
-        results.push("fizz");
-      }
-    }
-    else if (i % 5 === 0) {
-      if (i % 3 === 0) {
-        results.push("fizzbuzz");
-      } else {
-        results.push("buzz");
-      }
-    }
-    else {
-      results.push(i);
-    }
+  for (var i = 1; i < (maxNumber + 1); i++) {
+    // Think - "Does i divided by three not have a remainder?"
+    // Remember - when grouping or's in a not, all of them have to be false for the statement to be true
+    var toAppend = !(i % 3 || i % 5) ? "fizzbuzz" : !(i % 3) ? "fizz" : !(i % 5) ? "buzz" : i;
+    // var toAppend = i;
+    // if (i % 3 === 0 && i % 5 === 0) {
+    //   toAppend = "fizzbuzz";
+    // }
+    // else if (i % 3 === 0) {
+    //   toAppend = "fizz";
+    // }
+    // else if (i % 5 === 0) {
+    //   toAppend = "buzz";
+    // }
+    results.push(toAppend);
   }
 }
